bccdf2d551d8a8a50ab9aff251e7b834266c13c0
shuoyanjerry <jerryyanshuo@outlook.com>
2022-03-18 16:17:45 -0400

    v1.1

--- jewel.py
+++ jewel.py
@@ -170,9 +170,8 @@ class Jewel:
 
 
 if __name__ == "__main__":
-    port = 8080
+    port = int(os.environ.get('PORT', 33507))
     file_path = "./mywebsite/www"
-
     FR = FileReader()
 
     J = Jewel(port, file_path, FR)
